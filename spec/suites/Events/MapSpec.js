f3997236ff90fafe6de097e8cf9e496bbec0b804
Andrey Geonya <manufacturer.software@gmail.com>
2012-06-03 12:26:38 +0300

    Jasmine connected to require.js app

--- spec/suites/Events/MapSpec.js
+++ spec/suites/Events/MapSpec.js
@@ -1,28 +1,26 @@
-describe("Crystal.Events.Map", function()
-{
-    describe("initialize", function()
-    {
-        it("should be correct initialized", function()
-        {
-            var myMap = new Crystal.Map('myMap', {lat: 55.028936234826, lon: 82.927810142519}, 15);
-            new Crystal.Events.Map(myMap);
+define(['Events/Map', 'Map'], function(Events_Map, Map) {
+    describe("Events/Map", function() {
+        
+        describe("init", function() {
+            it("should be correct initialized", function() {
+                var myMap = new Map('myMap', {lat: 55.028936234826, lon: 82.927810142519}, 15);
+                new Events_Map(myMap);
+            });
+
+            it("should throw an error, because map param is invalid", function() {
+                expect(function() {
+                    new Events_Map({});
+                }).toThrow(new TypeError('Object passed to the initialize method of the Crystal.Events.Map class should be an instance of Crystal.Map.'));
+            });
         });
 
-        it("should throw an error, because map param is invalid", function()
-        {
-            expect(function() {
-                new Crystal.Events.Map({});
-            }).toThrow(new TypeError('Object passed to the initialize method of the Crystal.Events.Map class should be an instance of Crystal.Map.'));
+        describe("map", function() {
+            it("should return Map instance", function() {
+                var myMap = new Map('myMap', {lat: 55.028936234826, lon: 82.927810142519}, 15);
+                var mapEvent = new Events_Map(myMap);
+                expect(mapEvent.map instanceof Map).toBeTruthy();
+            });
         });
-    });
-    
-    describe("map", function()
-    {
-        it("should return Crystal.Map instance", function()
-        {
-            var myMap = new Crystal.Map('myMap', {lat: 55.028936234826, lon: 82.927810142519}, 15);
-            var mapEvent = new Crystal.Events.Map(myMap);
-            expect(mapEvent.map instanceof Crystal.Map).toBeTruthy();
-        });
+
     });
 });
