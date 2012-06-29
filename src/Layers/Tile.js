b4120d6db285962067c8859e3fb1f72138e5e4eb
Andrey Geonya <a.geonya@2gis.kiev.ua>
2012-06-29 21:34:14 +0300

    Layers/Tile/Dragging event fixed

--- src/Layers/Tile.js
+++ src/Layers/Tile.js
@@ -202,10 +202,7 @@ define([
                 var left = _containerOffset.x + event.currentPixel.x - event.startPixel.x;
                 var top = _containerOffset.y + event.currentPixel.y - event.startPixel.y;
 
-                Vendors_PubSub.publish('Layers/Tile/Dragging', event, {
-                    x: left,
-                    y: top
-                });
+                Vendors_PubSub.publish('Layers/Tile/Dragging', event);
 
                 _self.container.style.cssText += "; left: " + left + "px; top: " + top + "px;";
                 _drawer.redraw(left, top);
